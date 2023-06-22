19b77523d5b42298337f7b0195ba587888885a30
Ertan Taner <ertantaner2000@gmail.com>
2023-06-22 17:06:06 +0300

    feat: add projects section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
+  },
+  image: {
+    service: sharpImageService()
   }
 });
---
+++ src/interfaces/Project.ts
@@ -0,0 +1,7 @@
+export default interface Project {
+  title:string;
+  href:string;
+  description:string;
+  imgAlt: string;
+  image: ImageMetadata;
+}
